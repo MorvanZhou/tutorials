3adc53bc943196e09880eb4e4b5e79254d81d10b
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-05-31 21:35:46 +1000

    create basic25

---
+++ basic/25_import.py
@@ -0,0 +1,12 @@
+import time
+print(time.localtime())
+
+import time as t
+print(t.localtime())
+
+from time import localtime, time
+print(localtime())
+print(time())
+
+from time import *
+print(localtime())
--- tensorflow/tensorflow7_variable.py
+++ tensorflow/tensorflow7_variable.py
@@ -6,10 +6,10 @@ one = tf.constant(1)
 new_value = tf.add(state, one)
 update = tf.assign(state, new_value)
 
-init = tf.initialize_all_variables() # must have
+init = tf.initialize_all_variables()    # must have
 
 with tf.Session() as sess:
-  sess.run(init)  # run and activate
-  for _ in range(3):
-    sess.run(update)
-    print(sess.run(state))
+    sess.run(init)      # run and activate
+    for _ in range(3):
+        sess.run(update)
+        print(sess.run(state))
