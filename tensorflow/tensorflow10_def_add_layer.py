f53ad4f7cf53245c4caa6369ccdf0daa044aedf7
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-04 13:27:09 +1000

    create tensorflow10

---
+++ tensorflow/tensorflow10_def_add_layer.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
