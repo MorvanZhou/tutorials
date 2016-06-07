202c06dc84f2e79e20b594d69478412d15120b48
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-07 22:44:49 +1000

    create thread2

--- threading/thread2_add_thread.py
+++ threading/thread2_add_thread.py
@@ -1,11 +1,15 @@
 import threading
 
+#def main():
+#    print(threading.active_count())
+#    print(threading.enumerate()) # see the thread list
+#    print(threading.current_thread())
+
 def thread_job():
-    print('This is an added Thread, number is %s' % threading.current_thread())
+    print('This is a thread of %s' % threading.current_thread())
 
 def main():
-    added_thread = threading.Thread(target=thread_job)
-    added_thread.start()
-
+    thread = threading.Thread(target=thread_job,)
+    thread.start()
 if __name__ == '__main__':
     main()
