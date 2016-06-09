3ed2d4b366e9706e80ab635b1aa7126348d5195d
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-09 23:23:52 +1000

    create thread6

---
+++ threading/thread6_lock.py
@@ -0,0 +1,27 @@
+import threading
+
+def job1():
+    global A, lock
+    lock.acquire()
+    for i in range(10):
+        A += 1
+        print('job1', A)
+    lock.release()
+
+def job2():
+    global A, lock
+    lock.acquire()
+    for i in range(10):
+        A += 10
+        print('job2', A)
+    lock.release()
+
+if __name__ == '__main__':
+    lock = threading.Lock()
+    A = 0
+    t1 = threading.Thread(target=job1)
+    t2 = threading.Thread(target=job2)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
