a4921068ca7e20b73dc8aa436871196030489aaa
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-09 20:26:19 +1000

    create thread4

---
+++ threading/thread4_queue.py
@@ -0,0 +1,26 @@
+import threading
+import time
+from queue import Queue
+
+def job(l,q):
+    for i in range(len(l)):
+        l[i] = l[i]**2
+    q.put(l)
+
+def multithreading():
+    q = Queue()
+    threads = []
+    data = [[1,2,3],[3,4,5],[4,4,4],[5,5,5]]
+    for i in range(4):
+        t = threading.Thread(target=job, args=(data[i], q))
+        t.start()
+        threads.append(t)
+    for thread in threads:
+        thread.join()
+    results = []
+    for _ in range(4):
+          results.append(q.get())
+    print(results)
+
+if __name__ == '__main__':
+    multithreading()
