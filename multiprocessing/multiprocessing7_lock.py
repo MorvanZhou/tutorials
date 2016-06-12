58628c520226e4b856fca54ff15734c71ffa9964
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-12 13:40:48 +1000

    create multiprocessing7

---
+++ multiprocessing/multiprocessing7_lock.py
@@ -0,0 +1,26 @@
+import multiprocessing as mp
+import time
+
+def job(v, num, l):
+    l.acquire()
+    for _ in range(10):
+        time.sleep(0.1)
+        v.value += num
+        print(v.value)
+    l.release()
+
+def multicore():
+    l = mp.Lock()
+    v = mp.Value('i', 0)
+    p1 = mp.Process(target=job, args=(v, 1, l))
+    p2 = mp.Process(target=job, args=(v, 3, l))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+
+if __name__ == '__main__':
+    multicore()
+
+
+
