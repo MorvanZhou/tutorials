92996049869ef6407ae781e78959f713eb73f4a6
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-12 20:59:45 +1000

    create tk2

---
+++ tkinterTUT/tk2_label_button.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title('my window')
+window.geometry('200x100')
+
+var = tk.StringVar()
+l = tk.Label(window, textvariable=var, bg='green', font=('Arial', 12), width=15,
+             height=2)
+#l = tk.Label(root, text='OMG! this is TK!', bg='green', font=('Arial', 12), width=15, height=2)
+l.pack()
+
+on_hit = False
+def hit_me():
+    global on_hit
+    if on_hit == False:
+        on_hit = True
+        var.set('you hit me')
+    else:
+        on_hit = False
+        var.set('')
+
+b = tk.Button(window, text='hit me', width=15,
+              height=2, command=hit_me)
+b.pack()
+
+
+window.mainloop()
