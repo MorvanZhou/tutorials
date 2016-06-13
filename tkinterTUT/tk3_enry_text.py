5bf0a4d189525d94356abda21e9f648705515f1a
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-13 21:45:01 +1000

    create tk3

---
+++ tkinterTUT/tk3_enry_text.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title('my window')
+window.geometry('200x200')
+# e = tk.Entry(window, show="*")
+e = tk.Entry(window, show="1")
+e.pack()
+
+def insert_point():
+    var = e.get()
+    t.insert('insert', var)
+def insert_end():
+    var = e.get()
+    # t.insert('end', var)
+    t.insert(2.2, var)
+
+b1 = tk.Button(window, text='insert point', width=15,
+              height=2, command=insert_point)
+b1.pack()
+b2 = tk.Button(window, text='insert end',
+               command=insert_end)
+b2.pack()
+t = tk.Text(window, height=2)
+t.pack()
+
+window.mainloop()
