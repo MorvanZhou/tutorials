09f35b772830535dab985a62f462de4f6c0ca9af
Morvan <morvanzhou@gmail.com>
2016-06-14 10:40:13 +1000

    Update 29_zip_lambda_map.py

--- basic/29_zip_lambda_map.py
+++ basic/29_zip_lambda_map.py
@@ -1,3 +1,8 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
 a = [1,2,3]
 b = [4,5,6]
 
