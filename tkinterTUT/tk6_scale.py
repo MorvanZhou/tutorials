2fc024394bd5c92a1a5d47ca080cee8b1ddb7885
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-14 21:19:08 +1000

    create tk6

---
+++ tkinterTUT/tk6_scale.py
@@ -0,0 +1,22 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title('my window')
+window.geometry('200x200')
+
+l = tk.Label(window, bg='yellow', width=20, text='empty')
+l.pack()
+
+def print_selection(v):
+    l.config(text='you have selected ' + v)
+
+s = tk.Scale(window, label='try me', from_=5, to=11, orient=tk.HORIZONTAL,
+             length=200, showvalue=0, tickinterval=2, resolution=0.01, command=print_selection)
+s.pack()
+
+window.mainloop()
