cd174d437baf9c6ef09c37540e2433aaf681f4f7
Morvan <morvanzhou@gmail.com>
2016-06-14 10:39:58 +1000

    Update 28_try.py

--- basic/28_try.py
+++ basic/28_try.py
@@ -1,3 +1,8 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
 try:
     file = open('eeee', 'r+')
 except Exception as e:
