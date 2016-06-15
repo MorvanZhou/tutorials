c9e95f190fc1e4c56ce4b9c5727929c5aa63f6db
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-15 10:43:03 +1000

    create tk8

---
+++ tkinterTUT/tk8_canvas.py
@@ -0,0 +1,29 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title('my window')
+window.geometry('200x200')
+
+canvas = tk.Canvas(window, bg='blue', height=100, width=200)
+image_file = tk.PhotoImage(file='ins.gif')
+image = canvas.create_image(10, 10, anchor='nw', image=image_file)
+x0, y0, x1, y1= 50, 50, 80, 80
+line = canvas.create_line(x0, y0, x1, y1)
+oval = canvas.create_oval(x0, y0, x1, y1, fill='red')
+arc = canvas.create_arc(x0+30, y0+30, x1+30, y1+30, start=0, extent=180)
+rect = canvas.create_rectangle(100, 30, 100+20, 30+20)
+canvas.pack()
+
+def moveit():
+    canvas.move(rect, 0, 2)
+
+b = tk.Button(window, text='move', command=moveit).pack()
+
+
+window.mainloop()
+
