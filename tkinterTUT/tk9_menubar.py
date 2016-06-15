4b31743f792101bb8061993dd5ff8899fcf85b8a
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-15 11:35:30 +1000

    create tk9

---
+++ tkinterTUT/tk9_menubar.py
@@ -0,0 +1,39 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title('my window')
+window.geometry('200x200')
+
+l = tk.Label(window, text='', bg='yellow')
+l.pack()
+counter = 0
+def do_job():
+    global counter
+    l.config(text='do '+ str(counter))
+    counter+=1
+
+menubar = tk.Menu(window)
+filemenu = tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label='File', menu=filemenu)
+filemenu.add_command(label='New', command=do_job)
+filemenu.add_command(label='Open', command=do_job)
+filemenu.add_command(label='Save', command=do_job)
+filemenu.add_separator()
+filemenu.add_command(label='Exit', command=window.quit)
+
+editmenu = tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label='Edit', menu=editmenu)
+editmenu.add_command(label='Cut', command=do_job)
+editmenu.add_command(label='Copy', command=do_job)
+editmenu.add_command(label='Paste', command=do_job)
+
+window.config(menu=menubar)
+
+window.mainloop()
+
+
