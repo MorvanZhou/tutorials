f7f016e3332c0b8cbbd5e7a7ccb3f39b37a0b9e3
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-16 20:41:24 +1000

    create tk10

---
+++ tkinterTUT/tk10_frame.py
@@ -0,0 +1,32 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title('my window')
+window.geometry('200x200')
+tk.Label(window, text='on the window').pack()
+
+frm = tk.Frame(window)
+frm.pack()
+frm_l = tk.Frame(frm, )
+frm_r = tk.Frame(frm)
+frm_l.pack(side='left')
+frm_r.pack(side='right')
+
+tk.Label(frm_l, text='on the frm_l1').pack()
+tk.Label(frm_l, text='on the frm_l2').pack()
+tk.Label(frm_r, text='on the frm_r1').pack()
+window.mainloop()
+
+
+
+
+
+
+
+
+
