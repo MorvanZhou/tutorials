2c22923d29370e2914b1de8977abaf071d917dbe
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-17 10:21:59 +1000

    create basic34

---
+++ basic/34_pickle.py
@@ -0,0 +1,26 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import pickle
+
+a_dict = {'da': 111, 2: [23,1,4], '23': {1:2,'d':'sad'}}
+
+# pickle a variable to a file
+file = open('pickle_example.pickle', 'wb')
+pickle.dump(a_dict, file)
+file.close()
+
+# reload a file to a variable
+with open('pickle_example.pickle', 'rb') as file:
+    a_dict1 =pickle.load(file)
+
+print(a_dict1)
+
+
+
+
+
+
+
