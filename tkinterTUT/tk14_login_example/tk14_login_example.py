c1a62e0363fb073c2339d39a0bc85b5dfa771ef6
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-17 12:34:26 +1000

    create tk14

---
+++ tkinterTUT/tk14_login_example/tk14_login_example.py
@@ -0,0 +1,63 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import tkinter as tk
+import pickle
+
+window = tk.Tk()
+window.title('Welcome to Mofan Python')
+window.geometry('450x300')
+
+# welcome image
+canvas = tk.Canvas(window, height=200, width=500)
+image_file = tk.PhotoImage(file='welcome.gif')
+image = canvas.create_image(0,0, anchor='nw', image=image_file)
+canvas.pack(side='top')
+
+# user information
+tk.Label(window, text='User name: ').place(x=50, y= 150)
+tk.Label(window, text='Password: ').place(x=50, y= 190)
+
+var_usr_name = tk.StringVar()
+var_usr_name.set('example@python.com')
+entry_usr_name = tk.Entry(window, textvariable=var_usr_name)
+entry_usr_name.place(x=160, y=150)
+var_usr_pwd = tk.StringVar()
+entry_usr_pwd = tk.Entry(window, textvariable=var_usr_pwd, show='*')
+entry_usr_pwd.place(x=160, y=190)
+
+def usr_login():
+    usr_name = var_usr_name.get()
+    usr_pwd = var_usr_pwd.get()
+    try:
+        with open('usrs_info.pickle', 'rb') as usr_file:
+            usrs_info = pickle.load(usr_file)
+    except FileNotFoundError:
+        with open('usrs_info.pickle', 'wb') as usr_file:
+            usrs_info = {'admin': 'admin'}
+            pickle.dump(usrs_info, usr_file)
+    if usr_name in usrs_info:
+        if usr_pwd == usrs_info[usr_name]:
+            tk.messagebox.showinfo(title='Welcome', message='How are you? ' + usr_name)
+        else:
+            tk.messagebox.showerror(message='Error, your password is wrong, try again.')
+    else:
+        is_sign_up = tk.messagebox.askyesno('Welcome',
+                               'You have not sign up yet. Sign up today?')
+        if is_sign_up:
+            usr_sign_up()
+
+def usr_sign_up():
+    pass
+
+# login and sign up button
+btn_login = tk.Button(window, text='Login', command=usr_login)
+btn_login.place(x=170, y=230)
+btn_sign_up = tk.Button(window, text='Sign up', command=usr_sign_up)
+btn_sign_up.place(x=270, y=230)
+
+window.mainloop()
+
+
