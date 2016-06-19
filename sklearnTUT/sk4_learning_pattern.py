fa53a571327480d018a0ae402fead5a9f49d0597
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-19 22:13:38 +1000

    create sk4

---
+++ sklearnTUT/sk4_learning_pattern.py
@@ -0,0 +1,27 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+iris = datasets.load_iris()
+iris_X = iris.data
+iris_y = iris.target
+
+##print(iris_X[:2, :])
+##print(iris_y)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_X, iris_y, test_size=0.3)
+
+##print(y_train)
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+print(knn.predict(X_test))
+print(y_test)
+
+
