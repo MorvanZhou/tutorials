09a9ad9c7f635a49e8cd013b43ffbf33a6599005
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-19 19:29:00 +1000

    create tf7

--- tensorflowTUT/tensorflow7_variable.py
+++ tensorflowTUT/tensorflow7_variable.py
@@ -6,15 +6,17 @@
 import tensorflow as tf
 
 state = tf.Variable(0, name='counter')
+#print(state.name)
 one = tf.constant(1)
 
 new_value = tf.add(state, one)
 update = tf.assign(state, new_value)
 
-init = tf.initialize_all_variables()    # must have
+init = tf.initialize_all_variables()  # must have if define variable
 
 with tf.Session() as sess:
-    sess.run(init)      # run and activate
+    sess.run(init)
     for _ in range(3):
         sess.run(update)
         print(sess.run(state))
+
