edc92bd4e1c45833bad4b6087d3d14808daf2bcb
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-19 18:44:41 +1000

    create tf6

--- tensorflowTUT/tensorflow6_session.py
+++ tensorflowTUT/tensorflow6_session.py
@@ -5,11 +5,10 @@
 
 import tensorflow as tf
 
-matrix1 = tf.constant([[3,3]])
+matrix1 = tf.constant([[3, 3]])
 matrix2 = tf.constant([[2],
-                                   [2]])
-
-product = tf.matmul(matrix1, matrix2)  # np.dot()
+                       [2]])
+product = tf.matmul(matrix1, matrix2)  # matrix multiply np.dot(m1, m2)
 
 # method 1
 sess = tf.Session()
@@ -19,5 +18,10 @@ sess.close()
 
 # method 2
 with tf.Session() as sess:
-  result2 = sess.run(product)
-  print(result2)
+    result2 = sess.run(product)
+    print(result2)
+
+
+
+
+
