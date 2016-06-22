192ba8e5bae5a141464847eb1bab08788e8cb930
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-22 12:47:37 +1000

    create sk6

---
+++ sklearnTUT/sk6_model_attribute_method.py
@@ -0,0 +1,20 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+
+loaded_data = datasets.load_boston()
+data_X = loaded_data.data
+data_y = loaded_data.target
+
+model = LinearRegression()
+model.fit(data_X, data_y)
+
+print(model.predict(data_X[:4, :]))
+print(model.coef_)
+print(model.intercept_)
+print(model.get_params())
+print(model.score(data_X, data_y)) # R^2 coefficient of determination
