64ae00324f6144ae1ba36944794128efca167e48
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-22 11:48:44 +1000

    create sk5

---
+++ sklearnTUT/sk5_datasets.py
@@ -0,0 +1,28 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+loaded_data = datasets.load_boston()
+data_X = loaded_data.data
+data_y = loaded_data.target
+
+model = LinearRegression()
+model.fit(data_X, data_y)
+
+print(model.predict(data_X[:4, :]))
+print(data_y[:4])
+
+X, y = datasets.make_regression(n_samples=100, n_features=1, n_targets=1, noise=10)
+plt.scatter(X, y)
+plt.show()
+
+
+
+
+
+
