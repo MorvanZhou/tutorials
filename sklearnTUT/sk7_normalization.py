34cb67af8ac2038ea3084901a3309dbd9a4cdf44
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-22 16:33:55 +1000

    create sk7

---
+++ sklearnTUT/sk7_normalization.py
@@ -0,0 +1,43 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+from sklearn import preprocessing
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn.datasets.samples_generator import make_classification
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+
+a = np.array([[10, 2.7, 3.6],
+                     [-100, 5, -2],
+                     [120, 20, 40]], dtype=np.float64)
+print(a)
+print(preprocessing.scale(a))
+
+X, y = make_classification(n_samples=300, n_features=2 , n_redundant=0, n_informative=2,
+                           random_state=22, n_clusters_per_class=1, scale=100)
+plt.scatter(X[:, 0], X[:, 1], c=y)
+plt.show()
+X = preprocessing.scale(X)    # normalization step
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
+clf = SVC()
+clf.fit(X_train, y_train)
+print(clf.score(X_test, y_test))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
