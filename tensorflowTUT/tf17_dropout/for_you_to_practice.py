9f17ec19ed27d22280248716d0228e6d629d4224
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-23 21:21:34 +1000

    create tf17

---
+++ tensorflowTUT/tf17_dropout/for_you_to_practice.py
@@ -0,0 +1,58 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import tensorflow as tf
+from sklearn.datasets import load_digits
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+
+# load data
+digits = load_digits()
+X = digits.data
+y = digits.target
+y = LabelBinarizer().fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
+
+
+def add_layer(inputs, in_size, out_size, layer_name, activation_function=None, ):
+    # add one more layer and return the output of this layer
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, )
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b, )
+    tf.histogram_summary(layer_name + '/outputs', outputs)
+    return outputs
+
+
+# define placeholder for inputs to network
+
+xs = tf.placeholder(tf.float32, [None, 64])  # 8x8
+ys = tf.placeholder(tf.float32, [None, 10])
+
+# add output layer
+
+
+# the loss between prediction and real data
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
+                                              reduction_indices=[1]))  # loss
+tf.scalar_summary('loss', cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.Session()
+merged = tf.merge_all_summaries()
+# summary writer goes in here
+
+sess.run(tf.initialize_all_variables())
+
+for i in range(500):
+    sess.run(train_step, feed_dict={xs: X_train, ys: y_train})
+    if i % 50 == 0:
+        # record loss
+        pass
+
---
+++ tensorflowTUT/tf17_dropout/full_code.py
@@ -0,0 +1,65 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import tensorflow as tf
+from sklearn.datasets import load_digits
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+
+# load data
+digits = load_digits()
+X = digits.data
+y = digits.target
+y = LabelBinarizer().fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
+
+
+def add_layer(inputs, in_size, out_size, layer_name, activation_function=None, ):
+    # add one more layer and return the output of this layer
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, )
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    # here to dropout
+    Wx_plus_b = tf.nn.dropout(Wx_plus_b, keep_prob)
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b, )
+    tf.histogram_summary(layer_name + '/outputs', outputs)
+    return outputs
+
+
+# define placeholder for inputs to network
+keep_prob = tf.placeholder(tf.float32)
+xs = tf.placeholder(tf.float32, [None, 64])  # 8x8
+ys = tf.placeholder(tf.float32, [None, 10])
+
+# add output layer
+l1 = add_layer(xs, 64, 50, 'l1', activation_function=tf.nn.tanh)
+prediction = add_layer(l1, 50, 10, 'l2', activation_function=tf.nn.softmax)
+
+# the loss between prediction and real data
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
+                                              reduction_indices=[1]))  # loss
+tf.scalar_summary('loss', cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.Session()
+merged = tf.merge_all_summaries()
+# summary writer goes in here
+train_writer = tf.train.SummaryWriter("logs/train", sess.graph)
+test_writer = tf.train.SummaryWriter("logs/test", sess.graph)
+
+sess.run(tf.initialize_all_variables())
+
+for i in range(500):
+    # here to determine the keeping probability
+    sess.run(train_step, feed_dict={xs: X_train, ys: y_train, keep_prob: 0.5})
+    if i % 50 == 0:
+        # record loss
+        train_result = sess.run(merged, feed_dict={xs: X_train, ys: y_train, keep_prob: 1})
+        test_result = sess.run(merged, feed_dict={xs: X_test, ys: y_test, keep_prob: 1})
+        train_writer.add_summary(train_result, i)
+        test_writer.add_summary(test_result, i)
