465201feb19edc12949e93cb52cb08ed866087f5
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-23 13:24:11 +1000

    create sk9

---
+++ sklearnTUT/sk9_cross_validation2.py
@@ -0,0 +1,32 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+from sklearn.learning_curve import  learning_curve
+from sklearn.datasets import load_digits
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+import numpy as np
+
+digits = load_digits()
+X = digits.data
+y = digits.target
+train_sizes, train_loss, test_loss= learning_curve(
+        SVC(gamma=0.01), X, y, cv=10, scoring='mean_squared_error',
+        train_sizes=[0.1, 0.25, 0.5, 0.75, 1])
+train_loss_mean = -np.mean(train_loss, axis=1)
+test_loss_mean = -np.mean(test_loss, axis=1)
+
+plt.plot(train_sizes, train_loss_mean, 'o-', color="r",
+             label="Training")
+plt.plot(train_sizes, test_loss_mean, 'o-', color="g",
+             label="Cross-validation")
+
+plt.xlabel("Training examples")
+plt.ylabel("Loss")
+plt.legend(loc="best")
+plt.show()
+
+
+
