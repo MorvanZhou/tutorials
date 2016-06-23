5307f545f6d7b223a71ddc927381140190a1e3fe
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-23 18:10:57 +1000

    create sk10

---
+++ sklearnTUT/sk10_cross_validation3.py
@@ -0,0 +1,31 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+
+from sklearn.learning_curve import  validation_curve
+from sklearn.datasets import load_digits
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+import numpy as np
+
+digits = load_digits()
+X = digits.data
+y = digits.target
+param_range = np.logspace(-6, -2.3, 5)
+train_loss, test_loss = validation_curve(
+        SVC(), X, y, param_name='gamma', param_range=param_range, cv=10,
+        scoring='mean_squared_error')
+train_loss_mean = -np.mean(train_loss, axis=1)
+test_loss_mean = -np.mean(test_loss, axis=1)
+
+plt.plot(param_range, train_loss_mean, 'o-', color="r",
+             label="Training")
+plt.plot(param_range, test_loss_mean, 'o-', color="g",
+             label="Cross-validation")
+
+plt.xlabel("gamma")
+plt.ylabel("Loss")
+plt.legend(loc="best")
+plt.show()
