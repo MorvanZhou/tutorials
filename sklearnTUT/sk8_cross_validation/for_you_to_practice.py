c20c47272c87c9b06f683e68f26d3b107a098927
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-06-23 11:42:57 +1000

    create sk8

---
+++ sklearnTUT/sk8_cross_validation/for_you_to_practice.py
@@ -0,0 +1,34 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+from sklearn.datasets import load_iris
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# test train split #
+
+
+# this is cross_val_score #
+
+
+# this is how to use cross_val_score to choose model and configs #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sklearnTUT/sk8_cross_validation/full_code.py
@@ -0,0 +1,54 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+from sklearn.datasets import load_iris
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# test train split #
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=4)
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+print(knn.score(X_test, y_test))
+
+# this is cross_val_score #
+from sklearn.cross_validation import cross_val_score
+knn = KNeighborsClassifier(n_neighbors=5)
+scores = cross_val_score(knn, X, y, cv=5, scoring='accuracy')
+print(scores)
+
+# this is how to use cross_val_score to choose model and configs #
+from sklearn.cross_validation import cross_val_score
+import matplotlib.pyplot as plt
+k_range = range(1, 31)
+k_scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+##    loss = -cross_val_score(knn, X, y, cv=10, scoring='mean_squared_error') # for regression
+    scores = cross_val_score(knn, X, y, cv=10, scoring='accuracy') # for classification
+    k_scores.append(scores.mean())
+
+plt.plot(k_range, k_scores)
+plt.xlabel('Value of K for KNN')
+plt.ylabel('Cross-Validated Accuracy')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
