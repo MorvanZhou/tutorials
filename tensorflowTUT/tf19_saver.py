5959e334b0a3fe06e00accab75f8ae4e45c998bf
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-01 09:28:28 +1000

    create tf19

---
+++ tensorflowTUT/tf19_saver.py
@@ -0,0 +1,51 @@
+# View more python tutorials on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import tensorflow as tf
+import numpy as np
+
+## Save to file
+# remember to define the same dtype and shape when restore
+##W = tf.Variable([[1,2,3],[3,4,5]], dtype=tf.float32, name='weights')
+##b = tf.Variable([[1,2,3]], dtype=tf.float32, name='biases')
+##
+##init= tf.initialize_all_variables()
+##
+##saver = tf.train.Saver()
+##
+##with tf.Session() as sess:
+##    sess.run(init)
+##    save_path = saver.save(sess, "my_net/save_net.ckpt")
+##    print("Save to path: ", save_path)
+##
+
+################################################
+# restore variables
+# redefine the same shape and same type for your variables
+W = tf.Variable(np.arange(6).reshape((2, 3)), dtype=tf.float32, name="weights")
+b = tf.Variable(np.arange(3).reshape((1, 3)), dtype=tf.float32, name="biases")
+
+# not need init step
+
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    saver.restore(sess, "my_net/save_net.ckpt")
+    print("weights:", sess.run(W))
+    print("biases:", sess.run(b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
