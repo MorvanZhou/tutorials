adf182521b10582544900ea88675cac8079b007d
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-03 22:06:37 +1000

    create pd13

---
+++ numpy&pandas/13_set_value.py
@@ -0,0 +1,17 @@
+# View more python tutorials on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import pandas as pd
+import numpy as np
+
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=['A', 'B', 'C', 'D'])
+
+df.iloc[2,2] = 1111
+df.loc['2013-01-03', 'D'] = 2222
+df.A[df.A>0] = 0
+df['F'] = np.nan
+df['G']  = pd.Series([1,2,3,4,5,6], index=pd.date_range('20130101', periods=6))
+print(df)
