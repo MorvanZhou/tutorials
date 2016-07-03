07eb50b2cd4c83ffb29d186b42a04451574029aa
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-03 21:30:24 +1000

    create pd12

---
+++ numpy&pandas/12_selection.py
@@ -0,0 +1,31 @@
+# View more python tutorials on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import pandas as pd
+import numpy as np
+
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=['A', 'B', 'C', 'D'])
+
+print(df['A'], df.A)
+print(df[0:3], df['20130102':'20130104'])
+
+# select by label: loc
+print(df.loc['20130102'])
+print(df.loc[:,['A','B']])
+print(df.loc['20130102', ['A','B']])
+
+# select by position: iloc
+print(df.iloc[3])
+print(df.iloc[3, 1])
+print(df.iloc[3:5,0:2])
+print(df.iloc[[1,2,4],[0,2]])
+
+# mixed selection: ix
+print(df.ix[:3, ['A', 'C']])
+# Boolean indexing
+print(df[df.A > 0])
+
+
