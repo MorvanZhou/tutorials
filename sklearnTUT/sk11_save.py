ed924d32eb02cc0cb32174be59f89e23dc4ec354
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-03 16:21:24 +1000

    create sk11

---
+++ sklearnTUT/sk11_save.py
@@ -0,0 +1,30 @@
+# View more python tutorials on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+from sklearn import svm
+from sklearn import datasets
+
+clf = svm.SVC()
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+clf.fit(X, y)
+
+# method 1: pickle
+import pickle
+# save
+with open('save/clf.pickle', 'wb') as f:
+    pickle.dump(clf, f)
+# restore
+with open('save/clf.pickle', 'rb') as f:
+   clf2 = pickle.load(f)
+   print(clf2.predict(X[0:1]))
+
+# method 2: joblib
+from sklearn.externals import joblib
+# Save
+joblib.dump(clf, 'save/clf.pkl')
+# restore
+clf3 = joblib.load('save/clf.pkl')
+print(clf3.predict(X[0:1]))
