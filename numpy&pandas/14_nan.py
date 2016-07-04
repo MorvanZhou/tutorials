d46b7d54f3dd17fd1b2bc3e367e9902f9bfebc2f
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-04 21:45:28 +1000

    create pd14

---
+++ numpy&pandas/14_nan.py
@@ -0,0 +1,17 @@
+# View more python tutorials on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+
+import pandas as pd
+import numpy as np
+
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.arange(24).reshape((6,4)), index=dates, columns=['A', 'B', 'C', 'D'])
+
+df.iloc[0,1] = np.nan
+df.iloc[1,2] = np.nan
+print(df.dropna(axis=0, how='any'))   # how={'any', 'all'}
+print(df.fillna(value=0))
+print(pd.isnull(df))
