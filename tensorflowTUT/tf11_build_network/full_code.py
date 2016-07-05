3c72afd384399d497268942248a25d7dde4c39a3
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-05 20:18:10 +1000

    create tf 11

--- tensorflowTUT/tf11_build_network/full_code.py
+++ tensorflowTUT/tf11_build_network/full_code.py
@@ -5,6 +5,7 @@
 
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
@@ -17,9 +18,12 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
     return outputs
 
 # Make up some real data
-x_data = np.linspace(-1,1,300)[:, np.newaxis]
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
+
+##plt.scatter(x_data, y_data)
+##plt.show()
 
 # define placeholder for inputs to network
 xs = tf.placeholder(tf.float32, [None, 1])
@@ -30,13 +34,11 @@ l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
 prediction = add_layer(l1, 10, 1, activation_function=None)
 
 # the error between prediciton and real data
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
-                     reduction_indices=[1]))
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys-prediction), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
-
 # important step
 init = tf.initialize_all_variables()
-sess = tf.Session()
+sess= tf.Session()
 sess.run(init)
 
 for i in range(1000):
