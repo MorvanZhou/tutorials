6a76b1ada5185337fca9a9b26731b11924e51b70
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-05 19:48:45 +1000

    create tf 11

---
+++ tensorflowTUT/tf11_build_network/for_you_to_practice.py
@@ -0,0 +1,44 @@
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import tensorflow as tf
+import numpy as np
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
+
+
+# Make up some real data
+
+# define placeholder for inputs to network
+
+# add hidden layer
+
+# add output layer
+
+
+# the error between prediciton and real data
+
+# important step
+
+
+for i in range(1000):
+    # training
+    pass
+    if i % 50 == 0:
+        # to see the step improvement
+        pass
+
+
+
+
+
