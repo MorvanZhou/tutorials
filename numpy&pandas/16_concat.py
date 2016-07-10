d2bfa3ff80bd3a1a99c1e8b0acd45786185e4eee
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-10 16:10:32 +0800

    create pd 16

---
+++ numpy&pandas/16_concat.py
@@ -0,0 +1,35 @@
+# View more python tutorials on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+import pandas as pd
+import numpy as np
+
+# concatenating
+# ingnore index
+df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'])
+df2 = pd.DataFrame(np.ones((3,4))*1, columns=['a','b','c','d'])
+df3 = pd.DataFrame(np.ones((3,4))*2, columns=['a','b','c','d'])
+res = pd.concat([df1, df2, df3], axis=0, ignore_index=True)
+
+# join, ('inner', 'outer')
+df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'], index=[1,2,3])
+df2 = pd.DataFrame(np.ones((3,4))*1, columns=['b','c','d', 'e'], index=[2,3,4])
+res = pd.concat([df1, df2], axis=1, join='outer'))
+res = pd.concat([df1, df2], axis=1, join='inner')
+
+# join_axes
+res = pd.concat([df1, df2], axis=1, join_axes=[df1.index])
+
+# append
+df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'])
+df2 = pd.DataFrame(np.ones((3,4))*1, columns=['a','b','c','d'])
+df2 = pd.DataFrame(np.ones((3,4))*1, columns=['b','c','d', 'e'], index=[2,3,4])
+res = df1.append(df2, ignore_index=True)
+res = df1.append([df2, df3])
+
+s1 = pd.Series([1,2,3,4], index=['a','b','c','d']))
+res = df1.append(s1, ignore_index=True)
+
+print(res)
