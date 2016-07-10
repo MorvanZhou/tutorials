e4deb58222234ff9326e65c461bd74204d0a9eba
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-10 16:51:42 +0800

    create pd 16

--- numpy&pandas/16_concat.py
+++ numpy&pandas/16_concat.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 
 # concatenating
-# ingnore index
+# ignore index
 df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'])
 df2 = pd.DataFrame(np.ones((3,4))*1, columns=['a','b','c','d'])
 df3 = pd.DataFrame(np.ones((3,4))*2, columns=['a','b','c','d'])
