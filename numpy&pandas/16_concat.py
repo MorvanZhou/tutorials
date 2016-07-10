629a33440d559542f026a6b291c66ca0481150cf
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-10 16:12:00 +0800

    create pd 16

--- numpy&pandas/16_concat.py
+++ numpy&pandas/16_concat.py
@@ -16,7 +16,7 @@ res = pd.concat([df1, df2, df3], axis=0, ignore_index=True)
 # join, ('inner', 'outer')
 df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'], index=[1,2,3])
 df2 = pd.DataFrame(np.ones((3,4))*1, columns=['b','c','d', 'e'], index=[2,3,4])
-res = pd.concat([df1, df2], axis=1, join='outer'))
+res = pd.concat([df1, df2], axis=1, join='outer')
 res = pd.concat([df1, df2], axis=1, join='inner')
 
 # join_axes
@@ -29,7 +29,7 @@ df2 = pd.DataFrame(np.ones((3,4))*1, columns=['b','c','d', 'e'], index=[2,3,4])
 res = df1.append(df2, ignore_index=True)
 res = df1.append([df2, df3])
 
-s1 = pd.Series([1,2,3,4], index=['a','b','c','d']))
+s1 = pd.Series([1,2,3,4], index=['a','b','c','d'])
 res = df1.append(s1, ignore_index=True)
 
 print(res)
