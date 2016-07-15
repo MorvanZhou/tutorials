0076e797deb5c733ab4e8e0da3fcd04e77ba9441
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-15 14:53:29 +0800

    create pd 18

---
+++ numpy&pandas/18_plot.py
@@ -0,0 +1,28 @@
+# View more python tutorials on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+"""
+Please note, this code is only for python 3+. If you are using python 2+, please modify the code accordingly.
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# plot data
+
+# Series
+data = pd.Series(np.random.randn(1000), index=np.arange(1000))
+data = data.cumsum()
+##data.plot()
+
+# DataFrame
+data = pd.DataFrame(np.random.randn(1000, 4), index=np.arange(1000), columns=list("ABCD"))
+data = data.cumsum()
+# plot methods:
+# 'bar', 'hist', 'box', 'kde', 'area', scatter', hexbin', 'pie'
+ax = data.plot.scatter(x='A', y='B', color='DarkBlue', label="Class 1")
+data.plot.scatter(x='A', y='C', color='DarkGreen', label='Class 2', ax=ax)
+
+plt.show()
