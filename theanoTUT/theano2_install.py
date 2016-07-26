d19ab50f2d3b259bd6c5cfb21b4087ca4d3ec248
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-26 22:35:37 +0800

    create theano 2

---
+++ theanoTUT/theano2_install.py
@@ -0,0 +1,25 @@
+# View more python tutorials on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+# 2 - Install theano
+
+"""
+requirements:
+1. python 2 >=2.6 or python 3>=3.3
+2. Numpy >= 1.7.1
+3. Scipy >=0.11
+
+If using CPU, no other requirement.
+But if using GPU, you will need NVIDIA CUDA drivers and SDK.
+
+The must easy way to install theano is to use pip install.
+1. open your terminal (MacOS and Linux), or your command window (Windows)
+2. type "pip install theano" (for python 2x); type "pip3 install theano" (for python 3x)
+
+Note: to install theano on Windows machine may be a little bit stuggling. If you encounter any
+problem, please refer to this web page:
+http://deeplearning.net/software/theano/install_windows.html#install-windows
+
+"""
