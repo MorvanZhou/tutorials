c589787cb8dba34449ec0c975a6fbbbef573e2b5
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-29 14:55:55 +1000

    update theano TUT

--- theanoTUT/theano6_shared_variable.py
+++ theanoTUT/theano6_shared_variable.py
@@ -12,7 +12,7 @@ import theano
 import theano.tensor as T
 
 state = theano.shared(np.array(0,dtype=np.float64), 'state') # inital state = 0
-inc = T.scalar('inc', dtype=state.dtype)  # integer scalar  (dscalar = decimal scalar)
+inc = T.scalar('inc', dtype=state.dtype)
 accumulator = theano.function([inc], state, updates=[(state, state+inc)])
 
 # to get variable value
