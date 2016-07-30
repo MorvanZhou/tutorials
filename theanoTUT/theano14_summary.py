45df7d3e15e82b1d7eb6c0e281e0cc12135c8dcf
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-31 09:40:23 +1000

    update theano TUT

---
+++ theanoTUT/theano14_summary.py
@@ -0,0 +1,36 @@
+# View more python tutorials on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+# 14 - summary
+
+"""
+==============================
+Summary:
+-----------------------------------------------
+1. Understand the basic usage of Theano;
+2. Built a regression neural networks;
+3. Built a classification neural networks;
+4. Understand the overfitting and the solutions for solving this problem;
+5. Save your networks for future usage.
+
+==============================
+GPU computation:
+-----------------------------------------------
+Theano tutorial link: http://deeplearning.net/software/theano/tutorial/using_gpu.html
+Requirement: NVIDIA cards and CUDA backend
+
+==============================
+Theano Convolutional Neural Networks:
+----------------------------------------------
+Theano tutorial link: http://deeplearning.net/tutorial/lenet.html
+
+
+==============================
+Theano Recurrent Neural Networks:
+-----------------------------------------------
+Theano tutorial link: http://deeplearning.net/tutorial/rnnslu.html
+
+
+"""
