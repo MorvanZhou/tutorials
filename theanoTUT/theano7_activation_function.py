1a0ce883959c9857b4120434ec44fe48b85abed0
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-30 11:21:15 +1000

    update theano TUT

--- theanoTUT/theano7_activation_function.py
+++ theanoTUT/theano7_activation_function.py
@@ -11,8 +11,8 @@ http://deeplearning.net/software/theano/library/tensor/nnet/nnet.html
 
 The activation functions include but not limited to softplus, sigmoid, relu, softmax, elu, tanh...
 
-For the hidden layer, we could use relu, tanh...
-For classification problems, we could use softplus or softmax for the output layer.
+For the hidden layer, we could use relu, tanh, softplus...
+For classification problems, we could use sigmoid or softmax for the output layer.
 For regression problems, we could use a linear function for the output layer.
 
 """
