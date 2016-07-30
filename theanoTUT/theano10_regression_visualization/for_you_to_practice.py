03542750201cd7c5b5b4f336f13b04129d2793b4
Mofan Zhou <mofan.zhou@griffithuni.edu.au>
2016-07-30 15:14:24 +1000

    update theano TUT

--- theanoTUT/theano10_regression_visualization/for_you_to_practice.py
+++ theanoTUT/theano10_regression_visualization/for_you_to_practice.py
@@ -31,8 +31,8 @@ noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise        # y = x^2 - 0.5
 
 # show the fake data
-plt.scatter(x_data, y_data)
-plt.show()
+# plt.scatter(x_data, y_data)
+# plt.show()
 
 # determine the inputs dtype
 x = T.dmatrix("x")
