fe17b13cc966dd05d721711316e0a329089cc4cd
Morvan Zhou <morvanzhou@gmail.com>
2016-11-25 22:11:07 +1000

    fix bug

--- kerasTUT/5-classifier_example.py
+++ kerasTUT/5-classifier_example.py
@@ -23,8 +23,8 @@ from keras.optimizers import RMSprop
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 # data pre-processing
-X_train = X_train.reshape(X_train.shape[0], -1) / 255   # normalize
-X_test = X_test.reshape(X_test.shape[0], -1) / 255      # normalize
+X_train = X_train.reshape(X_train.shape[0], -1) / 256.   # normalize
+X_test = X_test.reshape(X_test.shape[0], -1) / 256.      # normalize
 y_train = np_utils.to_categorical(y_train, nb_classes=10)
 y_test = np_utils.to_categorical(y_test, nb_classes=10)
 
