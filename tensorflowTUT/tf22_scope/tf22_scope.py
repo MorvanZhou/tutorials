abc0362be0c6f66444cd1172c9c377b55da33af3
Morvan Zhou <morvanzhou@gmail.com>
2016-11-25 18:15:49 +1000

    add color bar

--- tensorflowTUT/tf22_scope/tf22_scope.py
+++ tensorflowTUT/tf22_scope/tf22_scope.py
@@ -32,7 +32,7 @@ with tf.variable_scope("a_variable_scope") as scope:
     var4 = tf.Variable(name='var4', initial_value=[4], dtype=tf.float32)
     var4_reuse = tf.Variable(name='var4', initial_value=[4], dtype=tf.float32)
     scope.reuse_variables()
-    var3_reuse = tf.get_variable(name='var3', shape=[1], dtype=tf.float32, initializer=initializer)
+    var3_reuse = tf.get_variable(name='var3',)
 
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
