c0bc82e5fdbbb11515830760b89ca586b243894c
Morvan <morvanzhou@gmail.com>
2017-03-07 15:27:53 +1100

    edit

--- Reinforcement_learning_TUT/5.1_Double_DQN/run_Pendulum.py
+++ Reinforcement_learning_TUT/5.1_Double_DQN/run_Pendulum.py
@@ -16,7 +16,7 @@ import tensorflow as tf
 env = gym.make('Pendulum-v0')
 env.seed(1)
 MEMORY_SIZE = 3000
-ACTION_SPACE = 5
+ACTION_SPACE = 11
 
 sess = tf.Session()
 with tf.variable_scope('Natural_DQN'):
@@ -42,7 +42,6 @@ def train(RL):
         action = RL.choose_action(observation)
 
         f_action = (action-(ACTION_SPACE-1)/2)/((ACTION_SPACE-1)/4)   # convert to [-2 ~ 2] float actions
-
         observation_, reward, done, info = env.step(np.array([f_action]))
 
         reward /= 10     # normalize to a range of (-1, 0). r = 0 when get upright
@@ -54,7 +53,7 @@ def train(RL):
         if total_steps > MEMORY_SIZE:   # learning
             RL.learn()
 
-        if total_steps - MEMORY_SIZE > 10000:   # stop game
+        if total_steps - MEMORY_SIZE > 20000:   # stop game
             break
 
         observation = observation_
