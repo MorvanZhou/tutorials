2048750aa7ed1763f3adb94062c9ac70929b7ea7
Morvan Zhou <morvanzhou@gmail.com>
2017-03-08 15:35:04 +1100

    edit

--- Reinforcement_learning_TUT/7_Policy_gradient_softmax/RL_brain.py
+++ Reinforcement_learning_TUT/7_Policy_gradient_softmax/RL_brain.py
@@ -73,6 +73,8 @@ class PolicyGradient:
             neg_log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=all_act, labels=self.tf_acts)   # this is negative log of chosen action
             # or in this way:
             # neg_log_prob = tf.reduce_sum(-tf.log(self.all_act_prob)*tf.one_hot(self.tf_acts, self.n_actions), axis=1)
+
+            # to maximize total reward (log_p * R) is to minimize -(log_p * R), and the tf only have minimize(loss)
             loss = tf.reduce_mean(neg_log_prob * self.tf_vt)  # reward guided loss
 
         with tf.name_scope('train'):
