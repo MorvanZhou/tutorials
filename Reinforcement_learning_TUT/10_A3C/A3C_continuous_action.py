4ee797b39a3e74400fb4abe4f5403bf7778bed20
Morvan Zhou <morvanzhou@gmail.com>
2017-03-31 02:36:49 +1100

    improve code

--- Reinforcement_learning_TUT/10_A3C/A3C_continuous_action.py
+++ Reinforcement_learning_TUT/10_A3C/A3C_continuous_action.py
@@ -42,31 +42,28 @@ A_BOUND = [env.action_space.low, env.action_space.high]
 
 
 class ACNet(object):
-    def __init__(self, scope, n_s, n_a,
-                 a_bound=None, sess=None,
-                 opt_a=None, opt_c=None, global_a_params=None, global_c_params=None):
+    def __init__(self, scope, globalAC=None):
 
         if scope == GLOBAL_NET_SCOPE:   # get global network
             with tf.variable_scope(scope):
-                self.s = tf.placeholder(tf.float32, [None, n_s], 'S')
-                self._build_net(n_a)
+                self.s = tf.placeholder(tf.float32, [None, N_S], 'S')
+                self._build_net(N_A)
                 self.a_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/actor')
                 self.c_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/critic')
         else:   # local net, calculate losses
-            self.sess = sess
             with tf.variable_scope(scope):
-                self.s = tf.placeholder(tf.float32, [None, n_s], 'S')
-                self.a_his = tf.placeholder(tf.float32, [None, n_a], 'A')
+                self.s = tf.placeholder(tf.float32, [None, N_S], 'S')
+                self.a_his = tf.placeholder(tf.float32, [None, N_A], 'A')
                 self.v_target = tf.placeholder(tf.float32, [None, 1], 'Vtarget')
 
-                mu, sigma, self.v = self._build_net(n_a)
+                mu, sigma, self.v = self._build_net(N_A)
 
                 td = tf.subtract(self.v_target, self.v, name='TD_error')
                 with tf.name_scope('c_loss'):
                     self.c_loss = tf.reduce_sum(tf.square(td))
 
                 with tf.name_scope('wrap_a_out'):
-                    mu, sigma = mu * a_bound[1], sigma + 1e-4
+                    mu, sigma = mu * A_BOUND[1], sigma + 1e-4
 
                 normal_dist = tf.contrib.distributions.Normal(mu, sigma)
 
@@ -78,7 +75,7 @@ class ACNet(object):
                     self.a_loss = -self.exp_v
 
                 with tf.name_scope('choose_a'):  # use local params to choose action
-                    self.A = tf.clip_by_value(tf.squeeze(normal_dist.sample(1), axis=0), a_bound[0], a_bound[1])
+                    self.A = tf.clip_by_value(tf.squeeze(normal_dist.sample(1), axis=0), A_BOUND[0], A_BOUND[1])
                 with tf.name_scope('local_grad'):
                     self.a_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/actor')
                     self.c_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/critic')
@@ -87,16 +84,16 @@ class ACNet(object):
 
             with tf.name_scope('sync'):
                 with tf.name_scope('pull'):
-                    self.pull_a_params_op = [l_p.assign(g_p) for l_p, g_p in zip(self.a_params, global_a_params)]
-                    self.pull_c_params_op = [l_p.assign(g_p) for l_p, g_p in zip(self.c_params, global_c_params)]
+                    self.pull_a_params_op = [l_p.assign(g_p) for l_p, g_p in zip(self.a_params, globalAC.a_params)]
+                    self.pull_c_params_op = [l_p.assign(g_p) for l_p, g_p in zip(self.c_params, globalAC.c_params)]
                 with tf.name_scope('push'):
-                    self.update_a_op = opt_a.apply_gradients(zip(self.a_grads, global_a_params))
-                    self.update_c_op = opt_c.apply_gradients(zip(self.c_grads, global_c_params))
+                    self.update_a_op = OPT_A.apply_gradients(zip(self.a_grads, globalAC.a_params))
+                    self.update_c_op = OPT_C.apply_gradients(zip(self.c_grads, globalAC.c_params))
 
     def _build_net(self, n_a):
         w_init = tf.random_normal_initializer(0., .1)
         with tf.variable_scope('actor'):
-            l_a = tf.layers.dense(self.s, 60, tf.nn.relu, kernel_initializer=w_init, name='la')
+            l_a = tf.layers.dense(self.s, 80, tf.nn.relu, kernel_initializer=w_init, name='la')
             mu = tf.layers.dense(l_a, n_a, tf.nn.tanh, kernel_initializer=w_init, name='mu')
             sigma = tf.layers.dense(l_a, n_a, tf.nn.softplus, kernel_initializer=w_init,
                                     bias_initializer=tf.constant_initializer(-0.5), name='sigma')
@@ -106,30 +103,29 @@ class ACNet(object):
         return mu, sigma, v
 
     def update_global(self, feed_dict):  # run by a local
-        self.sess.run([self.update_a_op, self.update_c_op], feed_dict)  # local grads applies to global net
+        SESS.run([self.update_a_op, self.update_c_op], feed_dict)  # local grads applies to global net
 
     def pull_global(self):  # run by a local
-        self.sess.run([self.pull_a_params_op, self.pull_c_params_op])
+        SESS.run([self.pull_a_params_op, self.pull_c_params_op])
 
     def choose_action(self, s):  # run by a local
         s = s[np.newaxis, :]
-        return self.sess.run(self.A, {self.s: s})[0]
+        return SESS.run(self.A, {self.s: s})[0]
 
 
 class Worker(object):
-    def __init__(self, env, name, n_s, n_a, a_bound, sess, opt_a, opt_c, g_a_params, g_c_params):
+    def __init__(self, env, name, globalAC):
         self.env = env
-        self.sess = sess
         self.name = name
-        self.AC = ACNet(name, n_s, n_a, a_bound, sess, opt_a, opt_c, g_a_params, g_c_params)
+        self.AC = ACNet(name, globalAC)
 
-    def work(self, update_iter, max_ep_step, gamma, coord):
+    def work(self):
         total_step = 1
         buffer_s, buffer_a, buffer_r = [], [], []
-        while not coord.should_stop() and GLOBAL_EP.eval(self.sess) < MAX_GLOBAL_EP:
+        while not COORD.should_stop() and GLOBAL_EP.eval(SESS) < MAX_GLOBAL_EP:
             s = self.env.reset()
             ep_r = 0
-            for ep_t in range(max_ep_step):
+            for ep_t in range(MAX_EP_STEP):
                 if self.name == 'W_0':
                     self.env.render()
                 a = self.AC.choose_action(s)
@@ -140,14 +136,14 @@ class Worker(object):
                 buffer_a.append(a)
                 buffer_r.append(r)
 
-                if total_step % update_iter == 0 or done:   # update global and assign to local net
+                if total_step % UPDATE_GLOBAL_ITER == 0 or done:   # update global and assign to local net
                     if done:
                         v_s_ = 0   # terminal
                     else:
-                        v_s_ = self.sess.run(self.AC.v, {self.AC.s: s_[np.newaxis, :]})[0, 0]
+                        v_s_ = SESS.run(self.AC.v, {self.AC.s: s_[np.newaxis, :]})[0, 0]
                     buffer_v_target = []
                     for r in buffer_r[::-1]:    # reverse buffer r
-                        v_s_ = r + gamma * v_s_
+                        v_s_ = r + GAMMA * v_s_
                         buffer_v_target.append(v_s_)
                     buffer_v_target.reverse()
 
@@ -163,47 +159,43 @@ class Worker(object):
 
                 s = s_
                 total_step += 1
-                if ep_t == max_ep_step-1:
+                if ep_t == MAX_EP_STEP-1:
                     print(
                         self.name,
-                        "Ep:", GLOBAL_EP.eval(self.sess),
+                        "Ep:", GLOBAL_EP.eval(SESS),
                         "| Ep_r: %.2f" % ep_r,
                           )
-                    sess.run(COUNT_GLOBAL_EP)
+                    SESS.run(COUNT_GLOBAL_EP)
                     break
 
 if __name__ == "__main__":
-    sess = tf.Session()
+    SESS = tf.Session()
 
     with tf.device("/cpu:0"):
         GLOBAL_EP = tf.Variable(0, dtype=tf.int32, name='global_ep', trainable=False)
         COUNT_GLOBAL_EP = tf.assign(GLOBAL_EP, tf.add(GLOBAL_EP, tf.constant(1), name='step_ep'))
         OPT_A = tf.train.RMSPropOptimizer(LR_A, name='RMSPropA')
         OPT_C = tf.train.RMSPropOptimizer(LR_C, name='RMSPropC')
-        globalAC = ACNet(GLOBAL_NET_SCOPE, N_S, N_A)  # we only need its params
+        GLOBAL_AC = ACNet(GLOBAL_NET_SCOPE)  # we only need its params
         workers = []
         # Create worker
         for i in range(N_WORKERS):
             i_name = 'W_%i' % i   # worker name
-            workers.append(
-                Worker(
-                    gym.make(GAME).unwrapped, i_name, N_S, N_A, A_BOUND, sess,
-                    OPT_A, OPT_C, globalAC.a_params, globalAC.c_params
-                ))
+            workers.append(Worker(gym.make(GAME).unwrapped, i_name, GLOBAL_AC))
 
-    coord = tf.train.Coordinator()
-    sess.run(tf.global_variables_initializer())
+    COORD = tf.train.Coordinator()
+    SESS.run(tf.global_variables_initializer())
 
     if OUTPUT_GRAPH:
         if os.path.exists(LOG_DIR):
             shutil.rmtree(LOG_DIR)
-        tf.summary.FileWriter(LOG_DIR, sess.graph)
+        tf.summary.FileWriter(LOG_DIR, SESS.graph)
 
     worker_threads = []
     for worker in workers:
-        job = lambda: worker.work(UPDATE_GLOBAL_ITER, MAX_EP_STEP, GAMMA, coord)
+        job = lambda: worker.work()
         t = threading.Thread(target=job)
         t.start()
         worker_threads.append(t)
-    coord.join(worker_threads)
+    COORD.join(worker_threads)
 
