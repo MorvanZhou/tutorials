c3c0888cab111b86f8bfcdda04b9743e788357a9
Morvan Zhou <morvanzhou@hotmail.com>
2017-04-10 12:16:45 +1000

    take mean of a_grads

--- Reinforcement_learning_TUT/1_command_line_reinforcement_learning/treasure_on_right.py
+++ Reinforcement_learning_TUT/1_command_line_reinforcement_learning/treasure_on_right.py
@@ -17,7 +17,7 @@ N_STATES = 6   # the length of the 1 dimensional world
 ACTIONS = ['left', 'right']     # available actions
 EPSILON = 0.9   # greedy police
 ALPHA = 0.1     # learning rate
-LAMBDA = 0.9    # discount factor
+GAMMA = 0.9    # discount factor
 MAX_EPISODES = 13   # maximum episodes
 FRESH_TIME = 0.3    # fresh time for one move
 
@@ -88,7 +88,7 @@ def rl():
             S_, R = get_env_feedback(S, A)  # take action & get next state and reward
             q_predict = q_table.ix[S, A]
             if S_ != 'terminal':
-                q_target = R + LAMBDA * q_table.iloc[S_, :].max()   # next state is not terminal
+                q_target = R + GAMMA * q_table.iloc[S_, :].max()   # next state is not terminal
             else:
                 q_target = R     # next state is terminal
                 is_terminated = True    # terminate this episode
