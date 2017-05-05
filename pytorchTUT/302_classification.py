744c9ffa2c1df61fb5245b3983bf3f5095dd9c38
Morvan Zhou <morvanzhou@hotmail.com>
2017-05-05 21:00:01 +1000

    fixed

--- pytorchTUT/302_classification.py
+++ pytorchTUT/302_classification.py
@@ -17,7 +17,7 @@ torch.manual_seed(1)    # reproducible
 n_data = torch.ones(100, 2)
 x0 = torch.normal(2*n_data, 1)      # class0 x data (tensor), shape=(100, 2)
 y0 = torch.zeros(100)               # class0 y data (tensor), shape=(100, 1)
-x1 = torch.normal(-2*n_data, 1)     # class1 x data (tensor), shape=(100, 1)
+x1 = torch.normal(-2*n_data, 1)     # class1 x data (tensor), shape=(100, 2)
 y1 = torch.ones(100)                # class1 y data (tensor), shape=(100, 1)
 x = torch.cat((x0, x1), 0).type(torch.FloatTensor)  # FloatTensor = 32-bit floating
 y = torch.cat((y0, y1), ).type(torch.LongTensor)    # LongTensor = 64-bit integer
