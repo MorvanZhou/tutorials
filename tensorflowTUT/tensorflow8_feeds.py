3c23b790d1aafd802a2dd0faa2ba1896ebc6b90f
Morvan Zhou <morvanzhou@gmail.com>
2017-06-05 23:30:43 +1000

    fix typo

--- tensorflowTUT/tensorflow8_feeds.py
+++ tensorflowTUT/tensorflow8_feeds.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 
 input1 = tf.placeholder(tf.float32)
 input2 = tf.placeholder(tf.float32)
-ouput = tf.multiply(input1, input2)
+output = tf.multiply(input1, input2)
 
 with tf.Session() as sess:
-    print(sess.run(ouput, feed_dict={input1: [7.], input2: [2.]}))
+    print(sess.run(output, feed_dict={input1: [7.], input2: [2.]}))
