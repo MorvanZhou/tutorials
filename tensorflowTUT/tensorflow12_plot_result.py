78e64f9da5834f4743c00a171f8af23a3876b14f
gbzhu <gbzhu@zju.edu.cn>
2017-07-17 09:26:48 +0800

    modify the spelling mistake and improving format

--- tensorflowTUT/tensorflow12_plot_result.py
+++ tensorflowTUT/tensorflow12_plot_result.py
@@ -11,6 +11,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def add_layer(inputs, in_size, out_size, activation_function=None):
     # add one more layer and return the output of this layer
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
@@ -22,8 +23,9 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
         outputs = activation_function(Wx_plus_b)
     return outputs
 
+
 # Make up some real data
-x_data = np.linspace(-1,1,300)[:, np.newaxis]
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
 
@@ -35,15 +37,15 @@ l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
 # add output layer
 prediction = add_layer(l1, 10, 1, activation_function=None)
 
-# the error between prediciton and real data
+# the error between prediction and real data
 loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
-                     reduction_indices=[1]))
+                                    reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 # important step
 # tf.initialize_all_variables() no long valid from
 # 2017-03-02 if using tensorflow >= 0.12
-if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+if int(tf.__version__.split('.')[1]) < 12 and int(tf.__version__.split('.')[0]) < 1:
     init = tf.initialize_all_variables()
 else:
     init = tf.global_variables_initializer()
@@ -52,7 +54,7 @@ sess.run(init)
 
 # plot the real data
 fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
+ax = fig.add_subplot(1, 1, 1)
 ax.scatter(x_data, y_data)
 plt.ion()
 plt.show()
@@ -70,7 +72,3 @@ for i in range(1000):
         # plot the prediction
         lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
         plt.pause(0.1)
-
-
-
-
