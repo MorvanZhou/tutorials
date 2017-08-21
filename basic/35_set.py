d217e982e110bd2ad7359d8cd2eb58524e42d560
Morvan Zhou <morvanzhou@gmail.com>
2017-08-21 20:49:11 +1000

    fix bug

--- basic/35_set.py
+++ basic/35_set.py
@@ -10,7 +10,7 @@ sentence = 'Welcome Back to This Tutorial'
 print(set(char_list))
 print(set(sentence))
 
-print(set([char_list, sentence]))
+print(set(char_list+ list(sentence)))
 
 unique_char = set(char_list)
 unique_char.add('x')
