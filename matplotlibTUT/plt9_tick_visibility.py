567cc961055f3b661f09600956353dbe1f265c02
Morvan Zhou <morvanzhou@hotmail.com>
2017-11-30 10:38:25 +1100

    update for new plt

--- matplotlibTUT/plt9_tick_visibility.py
+++ matplotlibTUT/plt9_tick_visibility.py
@@ -18,7 +18,7 @@ x = np.linspace(-3, 3, 50)
 y = 0.1*x
 
 plt.figure()
-plt.plot(x, y, linewidth=10)
+plt.plot(x, y, linewidth=10, zorder=1)      # set zorder for ordering the plot in plt 2.0.2 or higher
 plt.ylim(-2, 2)
 ax = plt.gca()
 ax.spines['right'].set_color('none')
@@ -32,5 +32,6 @@ ax.spines['left'].set_position(('data', 0))
 
 for label in ax.get_xticklabels() + ax.get_yticklabels():
     label.set_fontsize(12)
-    label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.7))
+    # set zorder for ordering the plot in plt 2.0.2 or higher
+    label.set_bbox(dict(facecolor='white', edgecolor='none', alpha=0.8, zorder=2))
 plt.show()
